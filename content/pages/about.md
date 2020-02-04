824a92a803e5b907c2889f695e17954af6954660
Ingo Fründ <mail@ingofruend.net>
2020-02-04 11:52:53 -0500

    initial

---
+++ content/pages/about.md
@@ -0,0 +1,9 @@
+Title: About Me
+Status: published
+
+<div style="text-align:center"><img style="width:50%;display:inline-block;" src="{static}/images/ingofruend.jpg" /></div>
+
+I have worked as a data scientist, an AI engineer and I am currently an <a href="http://www.yorku.ca/ifruend">Assistant Professor for Computational Neuroscience at York University, Toronto, ON</a>.
+This blog covers things that I am generally interested in; data science, machine learning but also music, rock climbing and whatever else I feel I should write about.
+
+Feel free to send me an email to: <span class="email">mail,ingofruend.net</span>
---
+++ pelicanconf.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Ingo Fruend'
+SITENAME = 'Ingo Fruend'
+SITEURL = 'https://ingofruend.net'
+
+PATH = 'content'
+
+TIMEZONE = 'America/Toronto'
+
+DEFAULT_LANG = 'en'
+
+THEME = 'mypelican'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ('Github', 'https://github.com/igordertigor'),
+ )
+
+# Social widget
+SOCIAL = (
+    ('Twitter', 'https://twitter.com/Ingo__Starr'),
+)
+
+DEFAULT_PAGINATION = 10
+DEFAULT_METADATA = {
+    'status': 'draft',
+}
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+# AUTHOR_SAVE_AS = ''  # Do not generate author pages
